3b4de2523e2a92c1ab0e39a7f7894ef3623afc67
Boof <97455552+hexahigh@users.noreply.github.com>
2023-11-19 13:48:16 +0100

    Update pic-10s.py

--- pic-10s.py
+++ pic-10s.py
@@ -1,15 +1,22 @@
 import time
-import picamera
+from picamera2 import Picamera2, Preview
 import os
 from datetime import datetime
 
-with picamera.PiCamera() as camera:
-    while True:
-        timestamp = datetime.now()
-        folder_path = os.path.join(str(timestamp.month), str(timestamp.day))
-        if not os.path.exists(folder_path):
-            os.makedirs(folder_path)
-        file_name = "{}.jpg".format(timestamp.strftime("%H:%M:%S"))
-        file_path = os.path.join(folder_path, file_name)
-        camera.capture(file_path)
-        time.sleep(10)
+picam = Picamera2()
+config = picam.create_preview_configuration()
+picam.configure(config)
+picam.start_preview(Preview.QTGL)
+picam.start()
+
+while True:
+    timestamp = datetime.now()
+    folder_path = os.path.join(str(timestamp.month), str(timestamp.day))
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    file_name = "{}.jpg".format(timestamp.strftime("%H_%M_%S"))
+    file_path = os.path.join(folder_path, file_name)
+    picam.capture_file(file_path)
+    time.sleep(10)
+
+picam.stop()
