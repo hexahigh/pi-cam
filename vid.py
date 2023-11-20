c70a85e2a9acd224698d6e2e813f068982a2aba1
Boof <97455552+hexahigh@users.noreply.github.com>
2023-11-20 21:03:30 +0100

    Update vid.py

--- vid.py
+++ vid.py
@@ -12,18 +12,21 @@ picam = Picamera2()
 config = picam.create_video_configuration()
 encoder = MJPEGEncoder()
 picam.configure(config)
-picam.start()
 
-while True:
-  
-  timestamp = datetime.now()
-  folder_path = os.path.join(str(timestamp.month), str(timestamp.day))
-  if not os.path.exists(folder_path):
+timestamp = datetime.now()
+folder_path = os.path.join(str(timestamp.month), str(timestamp.day))
+if not os.path.exists(folder_path):
     os.makedirs(folder_path)
-  file_name = "{}.jpg".format(timestamp.strftime("%H_%M_%S"))
-  file_path = os.path.join(folder_path, file_name)
-  picam.start_recording(encoder, file_path, Quality.HIGH)
-  logging.info('Captured video and saved to: {}'.format(file_path))
-  print('Captured video and saved to: {}'.format(file_path))
+file_name = "{}.jpg".format(timestamp.strftime("%H_%M_%S"))
+file_path = os.path.join(folder_path, file_name)
 
-picam.stop_recording()
+try:
+    picam.start_recording(encoder, file_path, Quality.HIGH)
+    logging.info('Started recording video to: {}'.format(file_path))
+    print('Started recording video to: {}'.format(file_path))
+    while True:
+        time.sleep(1)  # Wait for 1 second
+except KeyboardInterrupt:
+    picam.stop_recording()
+    logging.info('Stopped recording video.')
+    print('Stopped recording video.')
