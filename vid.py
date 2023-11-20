bf12481650e5b9002a4f16c8f91a86e5fbddb94a
Boof <97455552+hexahigh@users.noreply.github.com>
2023-11-20 20:59:29 +0100

    Create vid.py

---
+++ vid.py
@@ -0,0 +1,28 @@
+import time
+from picamera2 import Picamera2
+import os
+from datetime import datetime
+import logging
+
+# Set up logging
+logging.basicConfig(filename='picamera.log', level=logging.INFO, format='%(asctime)s %(message)s')
+
+picam = Picamera2()
+config = picam.create_video_configuration()
+encoder = JpegEncoder()
+picam.configure(config)
+picam.start()
+
+while True:
+  
+  timestamp = datetime.now()
+  folder_path = os.path.join(str(timestamp.month), str(timestamp.day))
+  if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+  file_name = "{}.jpg".format(timestamp.strftime("%H_%M_%S"))
+  file_path = os.path.join(folder_path, file_name)
+  picam.start_recording(encoder, file_path, Quality.HIGH)
+  logging.info('Captured video and saved to: {}'.format(file_path))
+  print('Captured video and saved to: {}'.format(file_path))
+
+picam.stop_recording()
