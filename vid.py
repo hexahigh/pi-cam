a91273f1b2b268f671064032476bb4ecf999902b
hexahigh <97455552+hexahigh@users.noreply.github.com>
2023-11-23 15:28:48 +0100

    b

--- vid.py
+++ vid.py
@@ -22,7 +22,7 @@ file_name = "{}.mp4".format(timestamp.strftime("%H_%M_%S"))
 file_path = os.path.join(folder_path, file_name)
 
 output1 = FfmpegOutput(file_path)
-output2 = FfmpegOutput()
+output2 = FfmpegOutput(file_path)
 encoder.output = [output1, output2]
 
 try:
