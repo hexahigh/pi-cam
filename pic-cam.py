1a35221c35ccb593601046ad3615783da1abfb71
hexahigh <97455552+hexahigh@users.noreply.github.com>
2023-11-25 14:08:46 +0100

    :(

--- pic-cam.py
+++ pic-cam.py
@@ -21,10 +21,11 @@ button_pin = 24
 led_pin = 23
 GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(led_pin, GPIO.OUT)
+GPIO.output(led_pin, GPIO.LOW)
 
 try:
     while True:
-        if GPIO.input(button_pin) == GPIO.HIGH:
+        if GPIO.input(button_pin) == 1:
             timestamp = datetime.now()
             folder_path = os.path.join(str(timestamp.month), str(timestamp.day))
             if not os.path.exists(folder_path):
